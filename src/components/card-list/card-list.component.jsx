2031a58ae710984210da97780f33708548cc5452
Tanuj Sharma <tanuj.khandel06@gmail.com>
2022-06-25 23:26:26 +0530

    v1 with searchbox

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./card-list.sytles.css";
+import { Card } from "../card/card.component";
+export const CardList = (props) => (
+  <div className='card-list'>
+    {props.monsters.map((monster) => (
+      <Card key={monster.id} monster={monster} />
+    ))}
+  </div>
+);
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./card.styles.css";
+export const Card = (props) => (
+  <div className='card-container'>
+    <img
+      src={`https://robohash.org/${props.monster.id}?set=set1&size=120x120`}
+      alt='monster'
+    />
+
+    <h2>{props.monster.name}</h2>
+    <p>{props.monster.email}</p>
+  </div>
+);
