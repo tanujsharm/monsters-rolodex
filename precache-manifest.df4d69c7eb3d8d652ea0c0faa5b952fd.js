9d8e93a0f19e0454d86d8c32c2b7b3ca06b97272
Tanuj Sharma <tanuj.khandel06@gmail.com>
2022-06-26 15:41:42 +0530

    Updates

---
+++ precache-manifest.df4d69c7eb3d8d652ea0c0faa5b952fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3189ef1f084fd29c576",
+    "url": "/monsters-rolodex/static/css/main.9a68b3ad.chunk.css"
+  },
+  {
+    "revision": "e3189ef1f084fd29c576",
+    "url": "/monsters-rolodex/static/js/main.40df1865.chunk.js"
+  },
+  {
+    "revision": "6c2d0b915730a523e189",
+    "url": "/monsters-rolodex/static/js/runtime~main.ade8ad79.js"
+  },
+  {
+    "revision": "5b1e7fa6d966527d9cbd",
+    "url": "/monsters-rolodex/static/js/2.d021738e.chunk.js"
+  },
+  {
+    "revision": "7b4b590765ba9cc9d7de",
+    "url": "/monsters-rolodex/static/js/3.f1bbbdc7.chunk.js"
+  },
+  {
+    "revision": "cfb93b48163e5852638d283457cade21",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
